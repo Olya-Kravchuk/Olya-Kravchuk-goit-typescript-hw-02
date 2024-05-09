cc2694a22a6de0d436ac70b4f638da19a624c888
Olya-Kravchuk <143799228+Olya-Kravchuk@users.noreply.github.com>
2024-05-09 11:12:30 +0200

    с

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,18 +1,21 @@
 import React, { FC } from "react";
 import { RotatingLines } from "react-loader-spinner";
 const Loader: FC = () => {
+
+  const props={
+    visible: true,
+    height: 96,
+    width:"96",
+    color:"green",
+    strokeWidth:"5",
+    animationDuration:"0.75",
+    ariaLabel:"rotating-lines-loading",
+  }
+
   return (
     <div>
       <RotatingLines
-        visible={true}
-        height="96"
-        width="96"
-        color="grey"
-        strokeWidth="5"
-        animationDuration="0.75"
-        ariaLabel="rotating-lines-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
+      {...props}
       />
     </div>
   );
